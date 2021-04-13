9badd9f990c31df4045527590e5c2a0fcd10bc0a
Tita Wijayanti <wijayantitita094@gmail.com>
2021-04-13 22:13:28 +0700

    Menambahkan method PUT pada view edit.blade.php karena articles.update hanya mendukung method PUT dan PATCH

--- resources/views/articles/edit.blade.php
+++ resources/views/articles/edit.blade.php
@@ -2,6 +2,7 @@
 @section('content')
     <div class="container">
         <form action="/articles/{{ $article->id }}" method="post" enctype="multipart/form-data">
+            @method('PUT')
             @csrf
             <div class="form-group">
                 <label for="title">Judul</label>
