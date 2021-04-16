1f3b78876c1d7ee340dfdb7cbbedb1b68d2676ec
Tita Wijayanti <wijayantitita094@gmail.com>
2021-04-16 23:17:35 +0700

    Meng-copy file migration dari jobsheet 9

---
+++ database/migrations/2021_04_09_171020_create_mahasiswa_matakuliah_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMahasiswaMatakuliahTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mahasiswa_matakuliah', function (Blueprint $table) {
+            $table->id();
+            $table->string('mahasiswa_id', 20)->nullable();
+            $table->unsignedBigInteger('matakuliah_id')->nullable();
+            $table->foreign('mahasiswa_id')->references('Nim')->on('mahasiswa');
+            $table->foreign('matakuliah_id')->references('id')->on('matakuliah');
+            $table->string('nilai');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mahasiswa_matakuliah');
+    }
+}
